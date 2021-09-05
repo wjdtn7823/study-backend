cef9f80d0fccaee53ad7f5702ce5d18eb837e753
davis.ad <davis.ad@kakaopaycorp.com>
2021-09-05 23:55:35 +0900

    project init

---
+++ src/test/kotlin/com/boo/study/StudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.boo.study
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StudyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
